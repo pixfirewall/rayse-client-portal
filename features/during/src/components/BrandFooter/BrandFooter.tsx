ab9ac85acb955d03cc946dcf033cce58b54c4a53
Allen Gainey <allen.gainey@rayse.com>
2024-05-09 13:10:47 +0000

    Merged in CPV-18-dev-during-create-brand-footer (pull request #11)
    CPV-18: create brand footer component
    
    * CPV-18: create brand footer component
    
    
    Approved-by: James Hancock

---
+++ features/during/src/components/BrandFooter/BrandFooter.tsx
@@ -0,0 +1,28 @@
+import './style.css'
+import React from 'react'
+import { Box, Text, YellowIcon, WhiteButton, Divider, Image } from '@rayseinc-packages/ui'
+
+import logo from './logo.png'
+
+export const BrandFooter = () => {
+  return (
+    <Box className="Brand-Footer-Container">
+      <Box className="Brand-Footer-Logo-CTA-Container">
+        <Image className="Brand-Footer-Logo" src={logo} width={96} height={96} />
+        <WhiteButton>
+          Learn more
+          <YellowIcon material="arrow_forward" />
+        </WhiteButton>
+      </Box>
+      <Divider className="Brand-Footer-Divider" orientation="horizontal" flexItem />
+      <Box className="Brand-Footer-Text-Container">
+        <Text variant="h6" color="#FFFFFF">
+          25% of buyers end up
+        </Text>
+        <Text variant="h6" color="#FFFFFF">
+          seeking a lawyer
+        </Text>
+      </Box>
+    </Box>
+  )
+}
