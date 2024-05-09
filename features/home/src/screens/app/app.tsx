481a2ee0697996f95144c6ab7991d73265f051c0
James Hancock <james.hancock@rayse.com>
2024-05-09 20:46:01 +0000

    Merged in CPV-27-add-buttons-to-main (pull request #19)
    Add buttons leading to pre and during pages
    
    * Add buttons to pre and during pages

--- features/home/src/screens/app/app.tsx
+++ features/home/src/screens/app/app.tsx
@@ -1,24 +1,26 @@
 import './styles/App.css'
 import React from 'react'
-import { Button, Box, Text, Link, Image } from '@rayseinc-packages/ui'
+import { Button, Box, Image } from '@rayseinc-packages/ui'
 
 import { useNavigateToDuring } from '@rayseinc-features/during'
+import { useNavigateToPre } from '@rayseinc-features/pre'
 
 import logo from './logo.svg'
 
 export const App = () => {
   const navigateToDuring = useNavigateToDuring()
+  const navigateToPre = useNavigateToPre()
 
   return (
     <Box className="App">
       <Box className="App-header">
         <Image src={logo} className="App-logo" alt="logo" />
-        <Text variant="h4">Edit `src/App.tsx` and save to reload.</Text>
-        <Link className="App-link" href="https://reactjs.org" target="_blank">
-          Learn React
-        </Link>
+
+        <Button variant="contained" onClick={() => navigateToPre()}>
+          Open the PRE page
+        </Button>
         <Button variant="contained" onClick={() => navigateToDuring()}>
-          Hello world
+          Open the DURING page
         </Button>
       </Box>
     </Box>
