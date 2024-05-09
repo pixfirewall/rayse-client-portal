bc646be622f9c2db18ead342ebe2f8e31221e3f1
Allen Gainey <allen.gainey@rayse.com>
2024-05-09 13:24:05 +0000

    Merged in CPV-20-integrate-components-with-during-01 (pull request #12)
    CPV-20: during integration
    
    * CPV-20: during integration
    
    
    Approved-by: James Hancock

--- features/during/src/screens/during/Main.tsx
+++ features/during/src/screens/during/Main.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { Box } from '@rayseinc-packages/ui'
+
+import './styles/main.css'
+import { Footer, BrandFooter } from '../../components'
 
 export const Main = () => {
-	return <></>
+  return (
+    <Box className="During-Container">
+      <Box className="During-Footer-Content">
+        <BrandFooter />
+        <Footer />
+      </Box>
+    </Box>
+  )
 }
